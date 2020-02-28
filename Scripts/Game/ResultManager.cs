92ab40ebb7dd2fce92780ef83af8561488451cbf
Dumble009 <daichi19990821@gmail.com>
2020-02-28 12:08:46 +0900

    Update ResultManager

--- Scripts/Game/ResultManager.cs
+++ Scripts/Game/ResultManager.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+public class RankingPacket
+{
+	public static int RankingCount = 5;
+	public int[] Ranks { get; set; }
+	public int[] Scores { get; set; }
+
+	public RankingPacket()
+	{
+		Ranks = new int[RankingCount];
+		Scores = new int[RankingCount];
+	}
+}
+
 public class ResultManager : SingletonMonoBehaviour<ResultManager>
 {
 	[SerializeField]
-	TextMeshProUGUI perfectCount, greatCount, goodCount, badCount, totalScore, firstRank, secondRank, thirdRank, fourthRank, fifthRank;
+	TextMeshProUGUI perfectCountTMPro, greatCountTMPro, goodCountTMPro, badCountTMPro, totalScoreTMPro;
+
+	public int PerfectCount { get; set; } = 0;
+	public int GreatCount { get; set; } = 0;
+	public int GoodCount { get; set; } = 0;
+	public int BadCount { get; set; } = 0;
+
+	[SerializeField]
+	TextMeshProUGUI[] rankingsTMPro;
 
 	[SerializeField]
 	GameObject resultRoot;
@@ -19,5 +40,13 @@ public class ResultManager : SingletonMonoBehaviour<ResultManager>
 	public void ShowResult()
 	{
 		resultRoot.SetActive(true);
+	}
+
+	public void SetRanking(RankingPacket packet)
+	{
+		for (int i = 0; i < RankingPacket.RankingCount; i++)
+		{
+			rankingsTMPro[i].text = string.Format(rankingsTMPro[i].text, packet.Ranks[i], packet.Scores[i]);
+		}
 	}
 }
