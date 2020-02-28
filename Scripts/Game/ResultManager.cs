0a51b935c7539f842ddc75686f90caa687cd1132
Dumble009 <daichi19990821@gmail.com>
2020-02-28 11:51:30 +0900

    Add ResultManager

---
+++ Scripts/Game/ResultManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResultManager : SingletonMonoBehaviour<ResultManager>
+{
+	[SerializeField]
+	TextMeshProUGUI perfectCount, greatCount, goodCount, badCount, totalScore, firstRank, secondRank, thirdRank, fourthRank, fifthRank;
+
+	[SerializeField]
+	GameObject resultRoot;
+
+	private void Start()
+	{
+		resultRoot.SetActive(false);
+	}
+
+	public void ShowResult()
+	{
+		resultRoot.SetActive(true);
+	}
+}
