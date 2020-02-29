c6832991982d0a174188a96ed86a9410ea8a35d8
Dumble009 <daichi19990821@gmail.com>
2020-02-29 23:05:49 +0900

    Make Ranking Enable

--- Scripts/Game/RankingManager.cs
+++ Scripts/Game/RankingManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using NCMB;
 using System.Linq;
 
-public class RankingManager : MonoBehaviour
+public class RankingManager : SingletonMonoBehaviour<RankingManager>
 {
 	NCMBQuery<NCMBObject> query;
+	public RankingPacket Packet { get; set; }
 	private void Start()
 	{
 		query = new NCMBQuery<NCMBObject>("Ranking");
@@ -14,16 +15,16 @@ public class RankingManager : MonoBehaviour
 		query.Limit = RankingPacket.RankingCount;
 
 		query.FindAsync((List<NCMBObject> objList, NCMBException e) => {
-			RankingPacket packet = new RankingPacket();
+			Packet = new RankingPacket();
 			for (int i = 0; i < RankingPacket.RankingCount; i++)
 			{
-				packet.Ranks[i] = 0;
-				packet.Scores[i] = 0;
+				Packet.Ranks[i] = 0;
+				Packet.Scores[i] = 0;
 			}
 
 			if (e != null)
 			{
-				ResultManager.Instance.SetRanking(packet);
+				ResultManager.Instance.SetRanking(Packet);
 			}
 			else
 			{
@@ -32,12 +33,20 @@ public class RankingManager : MonoBehaviour
 				{
 					rank++;
 					int score = System.Convert.ToInt32(obj["score"]);
-					packet.Ranks[rank - 1] = rank;
-					packet.Scores[rank - 1] = score;
+					Packet.Ranks[rank - 1] = rank;
+					Packet.Scores[rank - 1] = score;
 				}
 
-				ResultManager.Instance.SetRanking(packet);
+				//ResultManager.Instance.SetRanking(Packet);
 			}
 		});
+	}
+
+	public void SetScore(int score)
+	{
+		NCMBObject obj = new NCMBObject("Ranking");
+		obj["score"] = score;
+
+		obj.SaveAsync();
 	}
 }
--- Scripts/Game/ResultManager.cs
+++ Scripts/Game/ResultManager.cs
@@ -22,6 +22,7 @@ public class ResultManager : SingletonMonoBehaviour<ResultManager>
 {
 	[SerializeField]
 	TextMeshProUGUI perfectCountTMPro, greatCountTMPro, goodCountTMPro, badCountTMPro, totalScoreTMPro;
+	Dictionary<TextMeshProUGUI, string> defaultTexts;
 
 	public int PerfectCount { get; set; } = 0;
 	public int GreatCount { get; set; } = 0;
@@ -41,25 +42,65 @@ public class ResultManager : SingletonMonoBehaviour<ResultManager>
 		}
 	}
 
+	int changedIndex = -1;
+	IEnumerator textFlashAnimation;
+
 	protected override void Awake()
 	{
 		base.Awake();
 		restartSubject = new Subject<int>();
+		defaultTexts = new Dictionary<TextMeshProUGUI, string>();
 	}
 
 	private void Start()
 	{
 		resultRoot.SetActive(false);
+		defaultTexts.Add(perfectCountTMPro, perfectCountTMPro.text);
+		defaultTexts.Add(greatCountTMPro, greatCountTMPro.text);
+		defaultTexts.Add(goodCountTMPro, goodCountTMPro.text);
+		defaultTexts.Add(badCountTMPro, badCountTMPro.text);
+		defaultTexts.Add(totalScoreTMPro, totalScoreTMPro.text);
+
+		foreach (var ranking in rankingsTMPro)
+		{
+			defaultTexts.Add(ranking, ranking.text);
+		}
 		ProblemManager.Instance.OnProblemFinish
 			.Subscribe(_i => {
 				ShowResult();
+
+				PerfectCount = 0;
+				GreatCount = 0;
+				GoodCount = 0;
+				BadCount = 0;
+			});
+
+		ResultManager.Instance.OnRestart
+			.Subscribe(_i => {
+				ResetTMPros();
+				if (textFlashAnimation != null)
+				{
+					StopCoroutine(textFlashAnimation);
+					Color c = rankingsTMPro[changedIndex].color;
+					c.a = 1;
+					rankingsTMPro[changedIndex].color = c;
+				}
 			});
 	}
 
 	public void ShowResult()
 	{
 		resultRoot.SetActive(true);
+		//SetRanking(RankingManager.Instance.Packet);
+
 		int totalScore = PerfectCount * 1000 + GreatCount * 500 + GoodCount * 300 + BadCount * 0;
+
+		UpdateRanking(RankingManager.Instance.Packet, totalScore);
+		textFlashAnimation = RankingFlashAnimation();
+		StartCoroutine(textFlashAnimation);
+
+		RankingManager.Instance.SetScore(totalScore);
+
 		totalScoreTMPro.text = string.Format(totalScoreTMPro.text, totalScore);
 
 		perfectCountTMPro.text = string.Format(perfectCountTMPro.text, PerfectCount);
@@ -76,9 +117,64 @@ public class ResultManager : SingletonMonoBehaviour<ResultManager>
 		}
 	}
 
+	public void UpdateRanking(RankingPacket packet, int totalScore)
+	{
+		int tmpIndex = 0;
+		bool isChanged = false;
+		for (int i = 0; i < RankingPacket.RankingCount; i++)
+		{
+			if (!isChanged && packet.Scores[i] <= totalScore)
+			{
+				rankingsTMPro[i].text = string.Format(rankingsTMPro[i].text, packet.Ranks[i], totalScore);
+				isChanged = true;
+				changedIndex = i;
+			}
+			else
+			{
+				rankingsTMPro[i].text = string.Format(rankingsTMPro[i].text, packet.Ranks[i], packet.Scores[tmpIndex]);
+				tmpIndex++;
+			}
+		}
+
+		if (!isChanged)
+		{
+			changedIndex = -1;
+		}
+	}
+
 	public void Restart()
 	{
 		resultRoot.SetActive(false);
 		restartSubject.OnNext(0);
+	}
+
+	void ResetTMPros()
+	{
+		foreach (var tmpro in defaultTexts.Keys)
+		{
+			tmpro.text = defaultTexts[tmpro];
+		}
+	}
+
+	IEnumerator RankingFlashAnimation()
+	{
+		if (changedIndex != -1)
+		{
+			Color c = rankingsTMPro[changedIndex].color;
+			while (true)
+			{
+				c.a = 0;
+				rankingsTMPro[changedIndex].color = c;
+				yield return new WaitForSeconds(0.5f);
+
+				c.a = 1;
+				rankingsTMPro[changedIndex].color = c;
+				yield return new WaitForSeconds(0.5f);
+			}
+		}
+		else
+		{
+			yield break;
+		}
 	}
 }
