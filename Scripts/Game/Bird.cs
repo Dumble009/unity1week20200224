b6b7aa1794d768234d10675e5ce77da37a7b9286
Dumble009 <daichi19990821@gmail.com>
2020-03-01 15:06:58 +0900

    Add BGMManager

--- Scripts/Game/Bird.cs
+++ Scripts/Game/Bird.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class Bird : MonoBehaviour
 {
 	[SerializeField]
-	protected Sprite low, mid, high, def;
+	protected Sprite low, mid, high, def, perfectSprite, greatSprite, goodSprite, badSprite;
 	protected SpriteRenderer spriteRenderer;
 
 	protected virtual void Start()
 	{
 		spriteRenderer = GetComponent<SpriteRenderer>();
+		ScoreManager.Instance.OnCalcScore
+			.Subscribe(_s => {
+				switch (_s) {
+					case Score.PERFECT:
+						if (perfectSprite)
+						{
+							spriteRenderer.sprite = perfectSprite;
+						}
+						break;
+
+					case Score.GREAT:
+						if (greatSprite)
+						{
+							spriteRenderer.sprite = greatSprite;
+						}
+						break;
+
+					case Score.GOOD:
+						if (goodSprite)
+						{
+							spriteRenderer.sprite = goodSprite;
+						}
+						break;
+
+					case Score.BAD:
+						if (badSprite)
+						{
+							spriteRenderer.sprite = badSprite;
+						}
+						break;
+
+					default:
+						break;
+				}
+			});
+
+		StageManager.Instance.OnProblemBarStart
+			.Subscribe(_i => {
+				if (def)
+				{
+					spriteRenderer.sprite = def;
+				}
+			});
 	}
 
 	public virtual void ChangeSprite(int pitch)
---
+++ Scripts/Start/BGMManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class BGMManager : MonoBehaviour
+{
+	AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private void Start()
+	{
+		StageManager.Instance.OnStartStage
+			.Subscribe(_i => {
+				audioSource.Stop();
+			});
+	}
+}
